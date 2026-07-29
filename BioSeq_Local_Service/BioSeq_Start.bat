b875dfb698a45abcf08056cdb40838218e450925
ttmmss278-png <ttmmss278@gmail.com>
2026-07-29 18:05:19 +0800

    Add BioSeq local service launcher

---
+++ BioSeq_Local_Service/BioSeq_Start.bat
@@ -0,0 +1,11 @@
+@echo off
+
+title TMM BioSeq Analysis Engine
+
+echo ==============================
+echo TMM BioSeq Analysis Platform
+echo ==============================
+
+python ../backend/BioSeq_Server.py
+
+pause
